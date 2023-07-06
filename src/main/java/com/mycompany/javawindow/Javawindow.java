256ab5ba35930361c47ebe216f91ee82c0643044
willdev-crypto <wil.dgl@hotmail.com>
2023-07-06 16:52:48 -0300

    first commit

---
+++ src/main/java/com/mycompany/javawindow/Javawindow.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javawindow;
+
+/**
+ *
+ * @author wildg
+ */
+public class Javawindow {
+
+    public static void main(String[] args) {
+        System.out.println("Salve Mundo");
+    }
+}
